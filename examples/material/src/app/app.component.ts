5e0cd575aca0c9552b09bd40e9b3a3e36a30020f
ontrigger <firewarpcd@gmail.com>
2024-03-21 12:50:27 +0300

    add test component + ng cli

--- examples/material/src/app/app.component.ts
+++ examples/material/src/app/app.component.ts
@@ -1,16 +1,17 @@
-import { Component } from '@angular/core';
-import { AppService } from './app.service';
-import { MaterialModule } from './material.module';
+import {Component} from '@angular/core';
+import {AppService} from './app.service';
+import {MaterialModule} from './material.module';
+import {TestComponent} from "./test/test.component";
 
 @Component({
-  standalone: true,
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
-  imports: [MaterialModule],
+    standalone: true,
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    imports: [MaterialModule, TestComponent],
 })
 export class AppComponent {
-  constructor(private appService: AppService) {
-    console.log(appService.getHello());
-  }
+    constructor(private appService: AppService) {
+        console.log(appService.getHello());
+    }
 }
